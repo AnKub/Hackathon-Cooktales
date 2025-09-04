e9a95b57a9470769f4e338b4fc536779947166f0
AnKub <ankub3088@gmail.com>
2025-09-04 18:17:13 +0300

    make ai

--- cooktales/src/components/Header/Header.tsx
+++ cooktales/src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import './Header.scss';
+import React from 'react';
+import './Header.scss';
 
-// const Header: React.FC = () => (
-//   <header className="header-glass">
-//     <h1 className="header-title">Cooktales</h1>
-//     <div className="header-actions">
-//       <button className="header-theme-btn">🌗</button>
-//       <button className="header-auth-btn">Log In / Log Out</button>
-//     </div>
-//   </header>
-// );
+const Header: React.FC = () => (
+  <header className="header-glass">
+    <h1 className="header-title">Cooktales</h1>
+    <div className="header-actions">
+      <button className="header-theme-btn">🌗</button>
+      <button className="header-auth-btn">Log In / Log Out</button>
+    </div>
+  </header>
+);
 
-// export default Header;
+export default Header;
