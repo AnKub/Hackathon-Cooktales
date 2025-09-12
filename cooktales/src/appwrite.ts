c3bc255effd468516cbf3a58b65cfbdb1a563745
AnKub <ankub3088@gmail.com>
2025-09-12 14:49:09 +0200

    make searchbar

--- cooktales/src/appwrite.ts
+++ cooktales/src/appwrite.ts
@@ -1,9 +1,9 @@
-// filepath: src/appwrite.ts
+
 import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client();
 client
-  .setEndpoint('https://cloud.appwrite.io/v1') 
+  .setEndpoint('https://fra.cloud.appwrite.io/v1') 
   .setProject('68c2e2090030a8729312'); 
 
 export const account = new Account(client);
