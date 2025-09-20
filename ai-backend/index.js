6fa21ad95b217b16760e5061d6c460e9a68c007d
AnKub <ankub3088@gmail.com>
2025-09-20 09:43:02 +0200

    Clean

--- ai-backend/index.js
+++ ai-backend/index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { Configuration, OpenAIApi } = require('openai');
+const OpenAI = require("openai");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }));
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 app.post('/recipes', async (req, res) => {
   const { ingredients, mealType } = req.body;
