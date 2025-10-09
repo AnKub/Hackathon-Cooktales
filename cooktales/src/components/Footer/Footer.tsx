df041fda21e7e2bc80ee831dd7ec360d051c47a1
AnKub <ankub3088@gmail.com>
2025-10-09 17:21:40 +0200

    FavorieButton

--- cooktales/src/components/Footer/Footer.tsx
+++ cooktales/src/components/Footer/Footer.tsx
@@ -1,55 +1,55 @@
-// import React from 'react';
-// import './Footer.scss';
+import React from 'react';
+import './Footer.scss';
 
-// const Footer: React.FC = () => (
-//   <footer className="footer-glass">
+const Footer: React.FC = () => (
+  <footer className="footer-glass">
   
-//     <div className="footer-columns">  
-//       <div className="footer-logo">Cooktales</div>
-//       <div className="footer-column">
-//         <h4>About</h4>
-//         <ul>         
-//          <li>
-//           <a href="/about" className="footer-link">Story</a>
-//          </li>
-//           <li>
-//             <a href="https://github.com/AnKub" target="_blank" rel="noopener noreferrer">GitHub</a>
-//           </li>
-//           <li>
-//             <a href="www.linkedin.com/in/ankub88" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-//           </li>
-//           <li>
-//             <a href="ankub3088@email.com">Email</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <div className="footer-column">
-//         <h4>Recipes</h4>
-//         <ul>
-//           <li>Categories</li>
-//           <li>Popular</li>
-//         </ul>
-//       </div>
-//       <div className="footer-column">
-//         <h4>Support</h4>
-//         <ul>
-//           <li>Contact</li>
-//           <li>FAQ</li>
-//         </ul>
-//       </div>
-//       <div className="footer-column">
-//         <h4>Legal</h4>
-//         <ul>
-//           <li>Privacy</li>
-//           <li>Terms</li>
-//         </ul>
-//       </div>
-//     </div>
-//     <div className="footer-bottom">
-//       <span className="footer-author">Author: An</span>
-//       <span className="footer-made">Made with Paws</span>
-//     </div>
-//   </footer>
-// );
+    <div className="footer-columns">  
+      <div className="footer-logo">Cooktales</div>
+      <div className="footer-column">
+        <h4>About</h4>
+        <ul>         
+         <li>
+          <a href="/about" className="footer-link">Story</a>
+         </li>
+          <li>
+            <a href="https://github.com/AnKub" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </li>
+          <li>
+            <a href="www.linkedin.com/in/ankub88" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </li>
+          <li>
+            <a href="ankub3088@email.com">Email</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h4>Recipes</h4>
+        <ul>
+          <li>Categories</li>
+          <li>Popular</li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h4>Support</h4>
+        <ul>
+          <li>Contact</li>
+          <li>FAQ</li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h4>Legal</h4>
+        <ul>
+          <li>Privacy</li>
+          <li>Terms</li>
+        </ul>
+      </div>
+    </div>
+    <div className="footer-bottom">
+      <span className="footer-author">Author: An</span>
+      <span className="footer-made">Made with Paws</span>
+    </div>
+  </footer>
+);
 
-// export default Footer;
+export default Footer;
