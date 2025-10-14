06f7390f973e9a4f606483a03878f055abc9cda1
AnKub <ankub3088@gmail.com>
2025-10-14 18:34:10 +0200

    readme

--- cooktales/src/components/SearchBar/SearchBar.tsx
+++ cooktales/src/components/SearchBar/SearchBar.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import './SearchBar.scss';
+// import React from 'react';
+// import './SearchBar.scss';
 
-interface SearchBarProps {
-  value: string;
-  onChange: (value: string) => void;
-}
+// interface SearchBarProps {
+//   value: string;
+//   onChange: (value: string) => void;
+// }
 
-const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => (
-  <div className="search-bar">
-    <input
-      type="text"
-      placeholder="Search recipes..."
-      value={value}
-      onChange={e => onChange(e.target.value)}
-      className="search-bar__input"
-    />
-    {value && (
-      <button className="search-bar__clear" onClick={() => onChange('')} title="Clear">
-        ×
-      </button>
-    )}
-  </div>
-);
+// const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => (
+//   <div className="search-bar">
+//     <input
+//       type="text"
+//       placeholder="Search recipes..."
+//       value={value}
+//       onChange={e => onChange(e.target.value)}
+//       className="search-bar__input"
+//     />
+//     {value && (
+//       <button className="search-bar__clear" onClick={() => onChange('')} title="Clear">
+//         ×
+//       </button>
+//     )}
+//   </div>
+// );
 
-export default SearchBar;
+// export default SearchBar;
